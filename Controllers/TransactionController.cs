7e04a71226bbab102c777f60545d6475446e9a21
tingliu31 <NUGRU31@gmail.com>
2025-04-24 17:46:48 +0800

    -add TransactionController相關

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,34 @@
+﻿using Homework_SkillTree.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_SkillTree.Controllers
+{
+    public class TransactionController : Controller
+    {
+        public IActionResult Index()
+        {
+            var transactions = new List<TransactionViewModel>
+            {
+                new TransactionViewModel
+                {
+                    TransactionType = TransactionType.Expense,
+                    Date = new DateTime(2025, 1, 1),
+                    Amount = 300
+                },
+                new TransactionViewModel
+                {
+                    TransactionType = TransactionType.Income,
+                    Date = new DateTime(2025, 1, 2),
+                    Amount = 1600
+                },
+                 new TransactionViewModel
+                {
+                     TransactionType = TransactionType.Expense,
+                     Date = new DateTime(2025, 1, 3),
+                    Amount = 800
+                }
+            };
+            return View(transactions);
+        }
+    }
+}
---
+++ Models/TransactionViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Homework_SkillTree.Models
+{
+
+    public enum TransactionType
+    {
+        [Display(Name = "收入")]
+        Income,
+        [Display(Name = "支出")]
+        Expense
+    }
+
+    public class TransactionViewModel
+    {
+        public TransactionType TransactionType { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+
+}
