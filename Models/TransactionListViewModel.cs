d023cc78eb1e75c35124765b733725d6851f7a58
tingliu31 <NUGRU31@gmail.com>
2025-05-13 11:11:10 +0800

    -delete TransactionListViewModel

--- Models/TransactionListViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Homework_SkillTree.Models
-{
-    public class TransactionListViewModel
-    {
-        public List<TransactionViewModel> Transactions { get; set; }
-        public int CurrentPage { get; set; }
-        public int TotalPages { get; set; }
-    }
-}
